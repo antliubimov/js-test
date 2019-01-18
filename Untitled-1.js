738917636db771f59ba7466ec3520fab8821da41
Anton Liubimov <ant.liubimov@gmail.com>
2019-01-18 16:02:52 +0500

    orbitalPeriod function

--- Untitled-1.js
+++ Untitled-1.js
@@ -324,3 +324,21 @@
 // bob.setFullName("Haskell Curry");
 // console.log(bob.getFullName()); // Haskell Curry
 // console.log(bob.getLastName());  // Curry
+
+// function orbitalPeriod(arr) {
+//   var GM = 398600.4418; // the GM value of earth
+//   var earthRadius = 6367.4447; // the radius of the earth
+
+//   let newArr = [];
+
+//   arr.forEach(element => {
+//     let obj = {};
+//     obj['name'] = element['name'];
+//     obj['orbitalPeriod'] = Math.round(2 * Math.PI * Math.sqrt((element['avgAlt'] + earthRadius) ** 3 / GM));
+//     newArr.push(obj);
+//   });
+//   return newArr;
+// }
+
+// console.log(orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}])); //should return [{name: "sputnik", orbitalPeriod: 86400}].
+// console.log(orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])); // should return [{name : "iss", orbitalPeriod: 5557}, {name: "hubble", orbitalPeriod: 5734}, {name: "moon", orbitalPeriod: 2377399}].
