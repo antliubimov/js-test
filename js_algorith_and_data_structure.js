6968df84ca1a16f5d7a2e203121205e0428ff230
Anton Liubimov <ant.liubimov@gmail.com>
2019-01-21 16:53:32 +0500

    start function convertToRoman

--- js_algorith_and_data_structure.js
+++ js_algorith_and_data_structure.js
@@ -1,14 +1,23 @@
-function palindrome(str) {
-  // Good luck!
-  let newStr = str.split(/[\W+_]/g).join('').toLowerCase();
-  let reverseStr = newStr.split('').reverse().join('');
-  return (newStr === reverseStr) ? true : false;
+// function palindrome(str) {
+//   // Good luck!
+//   let newStr = str.split(/[\W+_]/g).join('').toLowerCase();
+//   let reverseStr = newStr.split('').reverse().join('');
+//   return (newStr === reverseStr) ? true : false;
+// }
+
+// console.log(palindrome("eye")); // true
+// console.log(palindrome("A man, a plan, a canal. Panama")); // true
+// console.log(palindrome("1 eye for of 1 eye.")); //false
+// console.log(palindrome("_eye")); //true
+// console.log(palindrome("0_0 (: /-\ :) 0-0")); //true
+
+function convertToRoman(num) {
+ let arr = String(num).split('');
+ return arr;
+
 }
 
-
-
-console.log(palindrome("eye")); // true
-console.log(palindrome("A man, a plan, a canal. Panama")); // true
-console.log(palindrome("1 eye for of 1 eye.")); //false
-console.log(palindrome("_eye")); //true
-console.log(palindrome("0_0 (: /-\ :) 0-0")); //true
+console.log(convertToRoman(36));
+console.log(convertToRoman(29)) // return "XXIX"
+console.log(convertToRoman(83)); //return "LXXXIII"
+console.log(convertToRoman(400)); //return "CD"
