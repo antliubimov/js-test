75e96e05f1e0ee99d0c4bfd3db50096cdb476dbb
Anton Liubimov <ant.liubimov@gmail.com>
2019-01-23 15:25:11 +0500

    function telephoneCheck

--- js_algorith_and_data_structure.js
+++ js_algorith_and_data_structure.js
@@ -30,23 +30,45 @@
 // console.log(convertToRoman(83)); //return "LXXXIII"
 // console.log(convertToRoman(400)); //return "CD"
 
-function rot13(str) { // LBH QVQ VG!
-  let newStr = '';
-  for (let i = 0; i < str.length; i += 1) {
-    let charCode = str.charCodeAt(i);
-    if (charCode >=65 && charCode <= 90) {
-      if (charCode >= 78) {
-        charCode -= 13;
-      } else {
-        charCode += 13;
-      }
-    }
-    newStr += String.fromCharCode(charCode);
-  }
-  return newStr;
+// function rot13(str) { // LBH QVQ VG!
+//   let newStr = '';
+//   for (let i = 0; i < str.length; i += 1) {
+//     let charCode = str.charCodeAt(i);
+//     if (charCode >=65 && charCode <= 90) {
+//       if (charCode >= 78) {
+//         charCode -= 13;
+//       } else {
+//         charCode += 13;
+//       }
+//     }
+//     newStr += String.fromCharCode(charCode);
+//   }
+//   return newStr;
+// }
+
+// // Change the inputs below to test
+// console.log(rot13("SERR PBQR PNZC")); // FREE CODE CAMP
+// console.log(rot13('SERR YBIR?')); //FREE LOVE?
+// console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); //THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+
+function telephoneCheck(str) {
+  // Good luck!
+  let regExp = /^([1]?\s?)?((\(\d{3}\)|\d{3})\s?[-]?)(\d{3}[-\s]?\d{4})$/;
+  return regExp.test(str);
 }
 
-// Change the inputs below to test
-console.log(rot13("SERR PBQR PNZC")); // FREE CODE CAMP
-console.log(rot13('SERR YBIR?')); //FREE LOVE?
-console.log(rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")); //THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+console.log(telephoneCheck("555-555-5555"));
+console.log(telephoneCheck("1 555-555-5555")); //true
+console.log(telephoneCheck("555-5555")); //false
+console.log(telephoneCheck("1 555)555-5555")); //false
+console.log(telephoneCheck("(6054756961)")); // false
+console.log(telephoneCheck("2 (757) 622-7382")); // false.
+console.log(telephoneCheck("0 (757) 622-7382")); // false.
+console.log(telephoneCheck("-1 (757) 622-7382")); // false
+console.log(telephoneCheck("2 757 622-7382")); // false.
+console.log(telephoneCheck("10 (757) 622-7382")); // false.
+console.log(telephoneCheck("27576227382")); // false.
+console.log(telephoneCheck("(275)76227382")); // false.
+console.log(telephoneCheck("2(757)6227382")); // false.
+console.log(telephoneCheck("2(757)622-7382")); // false.
+console.log(telephoneCheck("(555-555-5555")); // false.
